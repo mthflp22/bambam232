b2c8ef72240c9dcde5d1dfb788fb4a796e7986a3
mthflp22 <matheus.salvador2206@gmail.com>
2025-06-03 20:00:06 -0300

    feat: adiciona componente de lista de usuários

---
+++ src/components/ListaUsuarios.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Container
+} from '@mui/material';
+
+// Este componente mostra a lista de usuários cadastrados
+function ListaUsuarios({ usuarios }) {
+  return (
+    <Container maxWidth="md">
+      <Paper elevation={3} sx={{ p: 3, mt: 3 }}>
+        <Typography variant="h5" gutterBottom>
+          Usuários Cadastrados
+        </Typography>
+
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nome</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Idade</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {usuarios.map((usuario, index) => (
+                <TableRow key={index}>
+                  <TableCell>{usuario.nome}</TableCell>
+                  <TableCell>{usuario.email}</TableCell>
+                  <TableCell>{usuario.idade}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Container>
+  );
+}
+
+export default ListaUsuarios; 
